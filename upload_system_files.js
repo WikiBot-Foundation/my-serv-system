17470fd060873e774d4d42e24008e3c8628047e9
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-18 14:05:37 +0300

    Create upload_system_files.js

---
+++ upload_system_files.js
@@ -0,0 +1,29 @@
+<index>
+ //Attention: This Commands only ServerSQL Javascript Extension.//
+    get_record.file( ==> MYSERV
+   function.UpdateElementLog="MyServer.cilent"
+      javascript.engine
+          server.files(MyUpload( == this.cilent
+
+      import javascript('Upload_Data')
+         else.engine('data-upload')
+      
+      client.id('newOperator')
+         upload.server(files)
+   //Your can create a Upload Client.//
+         javascript.upload('data')
+ //Attention: Just enter your typed commands.//
+      cilent.key(03883G)
+        operator.javascript(file).upload
+  // The server runs as SQL with JavaScript 83.291.9 version.  Create a Function of cilent.token (system) == yourSystem.//
+     client.token(system) == 04729
+     sql.addresses(type == sql
+            engine.sysyem
+ 
+
+       
+      
+          
+
+
+     
