90f01dd12fcba50192083f3585f2a3140090f759
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-18 10:58:16 +0300

    Create MyServer.py

---
+++ MyServer.py
@@ -0,0 +1,49 @@
+## Custom Servers ##
+(add .py
+(add .c
+(add .cs
+(add .servertext
+(add .hostc
+(add .php
+(add .nah
+(add .js
+(add .githec
+(add .topic
+(add .custom
+(add .test
+(add .fs
+(add .folders
+(add .nameserver
+(add .webkey
+(add .lang
+(add .arscb
+    "'webkey="oyriRia:jeOryw;kfyaofOgiw"
+       folder="script.name-thisfold"
+       serverfold="orion.key"
+         web="server.key(thisfold)"
+        namely="web.orion"
+           testserv="custom.git"
+         gittiorit="nameserver"
+            "folder.file"
+            test.this(key == fun.fold
+         gitweb="inter.key"
+         servpy="gitserver"
+             lang="py"
+        orion-key="soft.py"
+        orion-key="soft.py-lang"
+        orion-key="soft.py-languag-file"
+        orion-key="soft.py="test.file"
+       orion-key="soft.py="JAVA-9372"
+          
+        public.orion( == key.py(server.file
+       public.soft(protonlang ==(server.h
+              public.py {Key.system(==client.id
+         server.cilent({this.key{[system.admin}
+             key.lang(soft== py
+
+    
+
+ 
+       
+
+ 
